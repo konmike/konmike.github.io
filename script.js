87c9405687185c224898e5e34c5b58e28dccccc8
konmike <konecmi4@fit.cvut.cz>
2019-05-04 23:04:41 +0200

    Porad nefunguje jquery, nevim proc

--- script.js
+++ script.js
@@ -12,19 +12,5 @@
         e.preventDefault();
     });
 
-/*    $(window).scroll(function() {
-        var top_of_element = $("#uvodnik").offset().top;
-        var bottom_of_element = $("#uvodnik").offset().top + $("#uvodnik").outerHeight();
-        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-        var top_of_screen = $(window).scrollTop();
-
-        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
-            $('#up').css('display', 'block');
-        } else {
-            $('#up').slideToggle('fast');
-        }
-    });*/
-
-
 
 })( jQuery );

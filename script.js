17fab3a19ef0ca3e9af555987a36c9e0a16ef09e
konmike <konecmi4@fit.cvut.cz>
2019-06-22 14:59:45 +0200

    Tlacitka k posunu nahoru a dolu

--- script.js
+++ script.js
@@ -9,15 +9,21 @@
     });
 
     $(window).scroll(function() {
-        var top_of_element = $("#myprojects").offset().top;
-        var bottom_of_element = $("#myprojects").offset().top + $("#myprojects").outerHeight();
+        var top_of_projects = $("#myprojects").offset().top;
+        var top_of_contact = $("#contact").offset().top;
+        var bottom_of_projects = $("#myprojects").offset().top + $("#myprojects").outerHeight();
+        var bottom_of_contact = $("#contact").offset().top + $("#contact").outerHeight();
         var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
         var top_of_screen = $(window).scrollTop();
 
-        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
-            $('#up').css('display', 'block');
-        } else {
-            $('#up').slideToggle('fast');
+        if ((bottom_of_screen > top_of_projects) && (top_of_screen < bottom_of_projects)){
+            $('#scroll-button').css('display', 'block');
+            $('#down').css('display', 'inline-block');
+        } else if((bottom_of_screen > top_of_contact) && (top_of_screen < bottom_of_contact)) {
+            $('#scroll-button').css('display', 'block');
+            $('#down').css('display', 'none');
+        }else {
+            $('#scroll-button').slideToggle('fast');
         }
     });
 

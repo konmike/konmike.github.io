222f1924c6670b187fa28a3fcc03d61437663672
konmike <konecmi4@fit.cvut.cz>
2019-06-22 14:40:32 +0200

    Upgrade slider

--- script.js
+++ script.js
@@ -8,5 +8,18 @@
         }, 1000);
     });
 
+    $(window).scroll(function() {
+        var top_of_element = $("#myprojects").offset().top;
+        var bottom_of_element = $("#myprojects").offset().top + $("#myprojects").outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
+
+        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+            $('#up').css('display', 'block');
+        } else {
+            $('#up').slideToggle('fast');
+        }
+    });
+
 
 })( jQuery );
